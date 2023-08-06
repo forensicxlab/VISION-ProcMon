fddefa8277f675620424d72a481992dcce9a5eee
k1nd0ne <k1nd0ne@mail.com>
2023-08-06 16:58:58 +0200

    Adding step range to move 'in time'

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as bootstrap from 'bootstrap';
+import "bootstrap-icons/font/bootstrap-icons.css";
 import { invoke } from "@tauri-apps/api/tauri";
 import $ from 'jquery';
 import './scss/styles.scss';
@@ -14,6 +15,9 @@ function print_error_message(message: string){
   const toast = new bootstrap.Toast('#error-toast'); 
   toast.show();
 }
+
+
+
 
 // function print_info_message(message: string){
 //   $('#info-message').text(message);  
@@ -60,31 +64,30 @@ function display_graph(json_graph: any){
       process = item;
     }
   })
-  // TODO : Create a case where we don't find the process (should not happend). 
 
-  graph.addNode(process.name, {type: "image", label: process.name, size: 10, color: "#FFFFFF"});
+  let steps_count = 0;
+  // Add the node and fill the process information on the card.
+  graph.addNode(process.name, {type: "image", label: process.name, size: 10, color: "#FFFFFF", step: steps_count});
 
   $('#card_process_name').text("Process Name: " + process.name);
   $('#card_process_pid').text("PID: " + process.pid);
   $('#card_process_ppid').text("PPID: " + process.ppid);
   $('#card_process_cmdline').text("Command Line : " + process.command_line);
-  
   $('#process_info_card').fadeIn();
-  let steps_count = 0;
+
   $.each(json_graph.nodes.nodes, function(_index, item){
     // Look for the parent process
     if (item.linked_pid == process.ppid && item.operation == "Process Create"){
       if (!graph.nodes().includes(item.name)){
-        graph.addNode(item.name, { label: item.name, size: 10, color: "#FFFFFF"});
+        graph.addNode(item.name, { label: item.name, size: 10, color: "#FFFFFF", step: steps_count});
         graph.addEdge(item.name, process.name, {type: "arrow", label: "Is Parent Process (" + steps_count + ")", size: 3, color: "#6f42c1"});
       }
-      
       steps_count++; 
     }
     // Display the Root Node Process.
     if (item.linked_pid == process.pid && item.operation == "Process Create"){
       if (!graph.nodes().includes(item.path)){
-        graph.addNode(item.path, {label: item.path, size: 10, color: "#FFFFFF", details: item.detail});
+        graph.addNode(item.path, {label: item.path, size: 10, color: "#FFFFFF", details: item.detail, step: steps_count});
         graph.addEdge(process.name, item.path, {type: "arrow", label: item.operation + "(" + steps_count + ")", size: 3, color: "#752a37"});
       }
       steps_count++; 
@@ -103,7 +106,7 @@ function display_graph(json_graph: any){
       }    
 
      if (!graph.nodes().includes(item.path)){
-        graph.addNode(item.path, {label: item.path, size: 10, color: "#FFFFFF"});
+        graph.addNode(item.path, {label: item.path, size: 10, color: "#FFFFFF", step: steps_count});
         graph.addEdge("QueryEA", item.path, {type: "arrow", label: item.operation + "(" + steps_count + ")", size: 3, color: "#e91e63"});
       }
       else{
@@ -126,7 +129,7 @@ function display_graph(json_graph: any){
       }    
 
       if (!graph.nodes().includes(item.path)){
-        graph.addNode(item.path, {label: item.path, size: 10, color: "#FFFFFF"});
+        graph.addNode(item.path, {label: item.path, size: 10, color: "#FFFFFF", step: steps_count});
         graph.addEdge("Read", item.path, {type: "arrow", label: item.operation + "(" + steps_count + ")", size: 3, color: "#2a5d75"});
 
       }
@@ -152,7 +155,7 @@ function display_graph(json_graph: any){
 
 
       if (!graph.nodes().includes(item.path)){
-        graph.addNode(item.path, {label: item.path, size: 10, color: "#FFFFFF"});
+        graph.addNode(item.path, {label: item.path, size: 10, color: "#FFFFFF", step: steps_count});
         graph.addEdge("Write", item.path, {type: "arrow", label: item.operation + "(" + steps_count + ")", size: 3, color: "#9c27b0"});
       }
       else{
@@ -174,7 +177,7 @@ function display_graph(json_graph: any){
       }
       
       if (!graph.nodes().includes(item.path)){
-        graph.addNode(item.path, {label: item.path, size: 10, color: "#FFFFFF", details: item.detail});
+        graph.addNode(item.path, {label: item.path, size: 10, color: "#FFFFFF", details: item.detail, step: steps_count});
         graph.addEdge("Key Creation", item.path, {type: "arrow", label: item.operation + "(" + steps_count + ")", size: 3, color: "#e91e63"});
       } 
       else{
@@ -198,22 +201,20 @@ function display_graph(json_graph: any){
           }
           
           if (!graph.nodes().includes(item.path)){
-            graph.addNode(item.path, {label: item.path, size: 10, color: "#FFFFFF", details: item.detail});
+            graph.addNode(item.path, {label: item.path, size: 10, color: "#FFFFFF", details: item.detail, step: steps_count});
             graph.addEdge("Key Deletion", item.path, {type: "arrow", label: item.operation + "(" + steps_count + ")", size: 3, color: "#e91e63"});
           } 
           else{
             graph.addEdge("Key Deletion", item.path, {type: "arrow", label: item.operation + "(" + steps_count + ")", size: 3, color: "#e91e63"});
           }
-          
           steps_count++; 
-    
         }
     
 
     // Create a node and and edge for the deleted registry key value
     if (item.linked_pid == linked_pid && item.operation == "RegDeleteValue" && $('#RegDeleteValue_filter').is(":checked")){
       if (!graph.nodes().includes("Registry")){
-        graph.addNode("Registry", {label: "Registry Hive", size: 10, color: "#FFFFFF"});
+        graph.addNode("Registry", {label: "Registry Hive", size: 12, color: "#FFFFFF"});
         graph.addEdge(selected_item, "Registry", {type: "arrow", label: "Interacts with", size: 3, color: "#000000"});
       }
 
@@ -223,7 +224,7 @@ function display_graph(json_graph: any){
       }
 
       if (!graph.nodes().includes(item.path)){
-        graph.addNode(item.path, {label: item.path, size: 10, color: "#FFFFFF", details: item.detail});
+        graph.addNode(item.path, {label: item.path, size: 10, color: "#FFFFFF", details: item.detail, step: steps_count});
         graph.addEdge("Delete Value", item.path, {type: "arrow", label: item.operation+ "(" + steps_count + ")", size: 3, color: "#0c797d"});
       }
       else{
@@ -246,7 +247,7 @@ function display_graph(json_graph: any){
       }
 
       if (!graph.nodes().includes(item.path)){
-        graph.addNode(item.path, {label: item.path, size: 10, color: "#FFFFFF", details: item.detail});
+        graph.addNode(item.path, {label: item.path, size: 10, color: "#FFFFFF", details: item.detail, step: steps_count});
         graph.addEdge("Set Value", item.path, {type: "arrow", label: item.operation + "(" + steps_count + ")", size: 3, color: "#d5572f"});
       } 
       else{
@@ -269,7 +270,7 @@ function display_graph(json_graph: any){
         graph.addEdge("Network", src, {type: "arrow", label: item.operation + "(" + steps_count + ")", size: 3, color: "#388e3c"});
       }
       if (!graph.nodes().includes(src_dst[1])){
-        graph.addNode(src_dst[1], {label: src_dst[1], size: 10, color: "#FFFFFF"});
+        graph.addNode(src_dst[1], {label: src_dst[1], size: 10, color: "#FFFFFF", step: steps_count});
         graph.addEdge(src, src_dst[1], {type: "arrow", label: "dst", size: 3, color: "#d5572f"});
       }
       steps_count++; 
@@ -290,19 +291,25 @@ function display_graph(json_graph: any){
         graph.addEdge("Network", src, {type: "arrow", label: item.operation + "(" + steps_count + ")", size: 3, color: "#00796b"});
       }
       if (!graph.nodes().includes(src_dst[1])){
-        graph.addNode(src_dst[1], {label: src_dst[1], size: 10, color: "#FFFFFF"});
+        graph.addNode(src_dst[1], {label: src_dst[1], size: 10, color: "#FFFFFF", step: steps_count});
         graph.addEdge(src, src_dst[1], {type: "arrow", label: "dst", size: 3, color: "#d5572f"});
       }
       steps_count++; 
     }
 
   });
+  steps_count--;
+  $("#steps_range").attr("max", steps_count);
+  $("#steps_range").val(steps_count);
+  $("#step_count").text(steps_count);
+
 
   graph.nodes().forEach((node, i) => {
     const angle = (i * 2 * Math.PI) / graph.order;
     graph.setNodeAttribute(node, "x", 100 * Math.cos(angle));
     graph.setNodeAttribute(node, "y", 100 * Math.sin(angle));
   });
+
  
   const renderer = new Sigma(graph, container, {
       // We don't have to declare edgeProgramClasses here, because we only use the default ones ("line" and "arrow")
@@ -313,10 +320,27 @@ function display_graph(json_graph: any){
       renderEdgeLabels: true,
       labelSize: 12,
       labelRenderer: drawHover,
-      allowInvalidContainer: false,
+      labelRenderedSizeThreshold: 1,
     });
 
-    
+
+
+    // Create the spring layout and start it
+    const layout = new FA2Layout(graph,{ settings: { gravity: 0.0001,}});
+    layout.start();
+    setTimeout(function(){
+      layout.stop();
+    }, 5000);
+
+    renderer.on("enterNode", (_e) => {
+      document.body.style.cursor = 'pointer';
+    });
+
+    renderer.on("leaveNode", (_e) => {
+      document.body.style.cursor = 'default';
+    });
+
+
     renderer.on("downNode", (e) => {
       $('#details_body').empty();
       $('#card_details').empty();
@@ -324,11 +348,10 @@ function display_graph(json_graph: any){
 
       if(attributes.label == "Write" || attributes.label == "Read" || attributes.label == "QueryEA" || attributes.label ==  "Key Creation" || attributes.label ==  "Delete Value" || attributes.label == "Set Value" || attributes.label ==  "Key Deletion"){
         for (const {attributes} of graph.neighborEntries(e.node)) {
-          console.log(attributes);
           const detail = document.createElement('span');
           detail.setAttribute('id','highlight-me');
           const br = document.createElement('br');
-          detail.textContent = attributes.label as string;
+          detail.textContent = attributes.step ? "("+attributes.step as string + ") : " + attributes.label as string : attributes.label as string;
           $("#details_body").append(detail);
           $("#details_body").append(br);
         }
@@ -353,19 +376,20 @@ function display_graph(json_graph: any){
 
     });
     
-
-    // Create the spring layout and start it
-    const layout = new FA2Layout(graph,
-      {
-        settings: {
-          gravity: 0.0001,
+    $("#steps_range").on("input", (e: any) => {
+      const step = e.target.value;
+      $("#step_count").text(step);
+      graph.nodes().map(function(item){
+        let attributes = graph.getNodeAttributes(item);
+        if (step < attributes.step){
+          graph.setNodeAttribute(item, "hidden", true);
         }
-      }      
-    );
-    setTimeout(function(){
-      layout.stop();
-    }, 5000);
-    layout.start();
+        else{
+          graph.setNodeAttribute(item, "hidden", false);
+        }
+      });
+    });
+
 }
 
 /* load : 
@@ -427,6 +451,11 @@ async function load(path: string){
   );
 }
 
+
+/*
+listen_drag :
+Listen for file drag and drop and update the UI 
+*/
 function listen_drag(){
   once('tauri://file-drop-hover', event => {
     $("#import_section").addClass("overlay");
@@ -478,24 +507,12 @@ function routine(){
   $('#import_section').removeClass("d-none");
 }
 
-$('#quit_btn').on("click", async function(){
-  location.reload();
-  routine();
-});
 
-document.addEventListener('DOMContentLoaded', () => {
-  routine();
-});
-
-
-
-// Make the DIV element draggable:
-dragElement(document.getElementById("process_info_card"));
-dragElement(document.getElementById("filter_card"));
-dragElement(document.getElementById("details_info"));
-
-
-function dragElement(elmnt: HTMLElement | null) {
+/*
+drag_cards : 
+Make a card draggable anywhere on the page.
+*/
+function drag_cards(elmnt: HTMLElement | null) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if(elmnt){
     var header = document.getElementById(elmnt.id + "_header")
@@ -511,7 +528,7 @@ function dragElement(elmnt: HTMLElement | null) {
     // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
+    document.onmouseup = close_drag_cards;
     // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
   }
@@ -531,13 +548,26 @@ function dragElement(elmnt: HTMLElement | null) {
     }
   }
 
-  function closeDragElement() {
+  function close_drag_cards() {
     // stop moving when mouse button is released:
     document.onmouseup = null;
     document.onmousemove = null;
   }
 }
 
+$('#quit_btn').on("click", async function(){
+  location.reload();
+  routine();
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  routine();
+});
+
+// Make the DIV element draggable:
+drag_cards(document.getElementById("process_info_card"));
+drag_cards(document.getElementById("filter_card"));
+drag_cards(document.getElementById("details_info"));
 
 $('#closebtn').on("click", async function(){
   $('#details_info').hide();
